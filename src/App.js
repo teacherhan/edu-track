4a55500585964744f1463ba5679057c1f6a8ff21
Erhan <teacherhan@gmail.com>
2020-08-25 02:43:13 -0700

    Table added to class list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import * as ReactBootStrap from "react-bootstrap";
 
 import "./App.css";
 //components
--- src/Components/ClassListOutput.js
+++ src/Components/ClassListOutput.js
@@ -1,13 +1,29 @@
 import React from "react";
 import "./ClassListOutput.css";
+import * as ReactBootStrap from "react-bootstrap";
 
 const classListOutput = (props) => {
   return (
     <div className="ClassListOutput">
       Class List <hr />
-      <p> {props.studentName}</p>
-      <p>{props.lastName}</p>
-      <p>{props.regClass}</p>
+      <ReactBootStrap.Table stripped bordered hover>
+        <thead>
+          <tr>
+            <th>NO#</th>
+            <th>FIRST NAME</th>
+            <th>LAST NAME</th>
+            <th>CLASS</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td> {props.studentName}</td>
+            <td>{props.lastName}</td>
+            <td>{props.regClass}</td>
+          </tr>
+        </tbody>
+      </ReactBootStrap.Table>
     </div>
   );
 };
